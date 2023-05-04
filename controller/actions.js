83930ef66acb1098d19fb11e6cf3c00744248c3e
Elihu Chitrit <eli@qwamo.com>
2023-05-04 10:38:55 +0300

    04052023-1

---
+++ controller/actions.js
@@ -0,0 +1,19 @@
+import express from 'express';
+const router = express.Router();
+
+
+
+
+
+router.get('/dashboard', async(req,res) => {
+    res.render('index', {
+
+    })
+})
+
+
+
+
+
+
+export default router;
---
+++ index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import database from './models/database.js';
+import dotenv from 'dotenv';
+import actions from './controller/actions.js';
+dotenv.config();
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
+app.use('/', actions);
+
+database
+.sync()
+.then(results => {
+    app.listen(process.env.PORT, () => {
+        console.log(`Server is running via port ${process.env.PORT}`);
+    })
+})
+.catch(err => {
+    console.log(err);
+})
